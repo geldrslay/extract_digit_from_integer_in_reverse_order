e1285ea49e6b7ed2d3ebad00b304d2b7899e7f60
Angela Ferrer <gelferrero01@gmail.com>
2024-01-26 04:53:25 +0800

    finalized

--- exercise_eleven.py
+++ exercise_eleven.py
@@ -3,3 +3,11 @@
 # Display the given number
 given_number =1024589
 print ('The given number is:',given_number)
+
+# Reverse given number using a while loop
+while given_number > 0:
+    digit = given_number % 10
+    given_number = given_number // 10
+    # Display the result 
+    print (digit, end=" ")
+    
